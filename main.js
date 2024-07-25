da830d457708d0fe629974557093418cf36e80e4
oSlavivna <hotyninfo@gmail.com>
2024-07-25 13:59:22 +0300

    tablet version + js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const btnClose = document.querySelector(".btn--close");
+const openBtn = document.querySelector(".open-btn");
+const sidebar = document.querySelector(".sidebar");
+
+btnClose.addEventListener("click", function () {
+  sidebar.classList.add("hidebar");
+});
+
+openBtn.addEventListener("click", function () {
+  sidebar.classList.remove("hidebar");
+  sidebar.classList.add("hidden");
+});
